0489d9998bf8e97cf3b5d0b70656efd642e45410
James Sanders <james.sanders@gtech.co.uk>
2022-11-16 15:46:15 +0000

    Remove css provided from upstream wireframe project

--- assets/js/pages/Events/Index.vue
+++ assets/js/pages/Events/Index.vue
@@ -5,9 +5,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <h2 class="text-3xl font-bold underline text-slate-500">
-    This is the events page
-  </h2>
+  <h2 class="text-3xl font-bold underlind">This is the events page</h2>
   <ul>
     <li v-for="(event, index) in events" :key="index">{{ event }}</li>
   </ul>
