2f97874adf17bc86450b8687af0de8650a61aaba
James Sanders <james.sanders@gtech.co.uk>
2022-11-21 14:56:17 +0000

    Nav component

--- assets/js/components/Layout.vue
+++ assets/js/components/Layout.vue
@@ -1,15 +1,10 @@
 <script setup>
-import { Link, Head } from "@inertiajs/inertia-vue3";
+import Nav from "./Nav.vue";
 </script>
 
 <template>
-  <nav>
-    <ul class="flex justify-items-center gap-5">
-      <li><Link href="/">Home</Link></li>
-      <li><Link href="/contact/">Contact</Link></li>
-    </ul>
-  </nav>
   <div class="text-3xl font-bold underline">
+    <Nav />
     <slot />
   </div>
 </template>
---
+++ assets/js/components/Nav.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+</script>
+
+<template>
+  <nav class>
+    <ul class="flex justify-items-center gap-5">
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/contact/">Contact</Link></li>
+    </ul>
+  </nav>
+</template>
